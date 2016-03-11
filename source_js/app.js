8ca934e8eebbc3bf68e08124927cb0aa7eddd1cd
Eric NG <kinyanng@yahoo.com.hk>
2016-03-11 03:01:42 +0000

    mp3 submission

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,20 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3', ['ngRoute']);
 
 app.config(function ($routeProvider) {
-
-})
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/list.html',
+            controller: 'listController'
+        })
+        .when('/gallery', {
+            templateUrl: 'partials/gallery.html',
+            controller: 'galleryController'
+        })
+        .when('/details', {
+            templateUrl: 'partials/details.html',
+            controller: 'detailsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,60 @@
 /* Sample Controller */
+app
+    .run(['$rootScope', '$http', function ($rootScope, $http) {
+        $http
+            .get('data/imdb250.json')
+            .success(function (data) {
+                $rootScope.movies = data;
+            })
+            .error(function (err) {
+                console.log(err);
+            });
+
+        $rootScope.genres = [
+            '',
+            'Action',
+            'Adventure',
+            'Animation',
+            'Biography',
+            'Comedy',
+            'Crime',
+            'Drama',
+            'Family',
+            'Fantasy',
+            'Film-Noir',
+            'History',
+            'Horror',
+            'Music',
+            'Musical',
+            'Mystery',
+            'Romance',
+            'Sci-Fi',
+            'Sport',
+            'Thriller',
+            'War',
+            'Western'
+        ];
+    }])
+    .controller('listController', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $scope.movies = $rootScope.movies;
+        $scope.predicate = 'rank';
+        $scope.order = 'asc';
+    }])
+    .controller('galleryController', ['$rootScope', '$scope', function ($rootScope, $scope) {
+        $scope.movies = $rootScope.movies;
+        $scope.genres = $rootScope.genres;
+        $scope.selectedGenre = '';
+        $scope.changeGenre = function (genre) {
+            console.log(genre);
+            $scope.selectedGenre = genre;
+        }
+    }])
+    .controller('detailsController', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+        var rank = $location.search().rank;
+        for (var i = 0; i < $rootScope.movies.length; i++) {
+            if ($rootScope.movies[i].rank == rank) {
+                $scope.movie = $rootScope.movies[i];
+                break;
+            }
+        }
+    }]);
